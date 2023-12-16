06f4ffb28162fb9045ef5f622f221e2f77d625f6
Uppicki <81292510+Uppicki@users.noreply.github.com>
2023-12-16 19:24:17 +0300

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:untitled4/pages/todo_list_page/todo_list_page.dart';
+
+class TODOApp extends StatelessWidget {
+  const TODOApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+              fontSize: 24, height: 32 / 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+      home: ,
+    );
+  }
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:untitled4/app.dart';
+
+void main() {
+  runApp(const TODOApp());
+}
+
+
